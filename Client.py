7caf5f5bb519b0b647b7f6f7043f1b0bd8ab675c
Martian <scliqiao@qq.com>
2013-11-24 00:04:32 +0800

    Ver2.0

--- Client.py
+++ Client.py
@@ -1,58 +1,70 @@
 import serial
 import socket
 import threading
-import time
 
-global ser,soc
 def InitSerial():
     dev0="/dev/ttyUSB0"
+    dev1="/dev/ttyUSB1"
     baud = 9600
-    ser=serial.Serial(dev0,baud)
+    while(True):
+        try:
+            ser=serial.Serial(dev0,baud)
+        except:
+            try:
+                ser=serial.Serial(dev1,baud)
+            except:
+                continue
+        break
     return ser
 def InitSocket():
     host = '192.168.1.20'
     port = 1235
-    soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
-    soc.connect((host,port))
+    while(True):
+        try:
+            soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+        except socket.error:
+            continue
+        try:
+            soc.connect((host,port))
+        except socket.error:
+            continue
+        except socket.timeout:
+            continue
+        break
     return soc
-class SockettoSerial(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.thread_stop = False
-    def run(self): #Overwrite run() method, put what you want the thread do here
+def SockettoSerial():
+    soc = InitSocket()
+    ser = InitSerial()
+    while True:
         soc = InitSocket()
-        ser = InitSerial()
-        while not self.thread_stop:
-            soc = InitSocket()
-            soc_msg = soc.recv(1024)
-            if len(soc_msg)!=0:
-                if soc_msg[-1]=='\n':
-                    print soc_msg
-                    ser.write(soc_msg)
-    def stop(self):
-        self.thread_stop = True
-class SerialtoScoket(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.thread_stop = False
-    def run(self):
-        ser=InitSerial()
-        while not self.thread_stop:
-            ser.flushInput()
-            ser_msg = ser.readline()
-            if len(ser_msg)!=0:
-                print ser_msg
-                soc=InitSocket()
-                soc.send(ser_msg)
-                soc.close()
-    def stop(self):
-        self.thread_stop = True
+        soc_msg = soc.recv(1024)
+        if len(soc_msg)!=0:
+            if soc_msg[-1]=='\n':
+                print soc_msg
+                while(True):
+                    try:
+                        ser.write(soc_msg)
+                    except:
+                        continue
+                    break
+def SerialtoScoket():
+    ser=InitSerial()
+    while True:
+        ser.flushInput()
+        ser_msg = ser.readline()
+        if len(ser_msg)!=0:
+            print ser_msg
+            soc=InitSocket()
+            while(True):
+                try:
+                    soc.send(ser_msg)
+                except:
+                    continue
+                break
+            soc.close()
+                      
 if __name__ == '__main__':
-    thread1=SockettoSerial()
-    thread2=SerialtoScoket()
+    thread1=threading.Thread(target=SockettoSerial)
+    thread2=threading.Thread(target=SerialtoScoket)
     thread1.start()
     thread2.start()
-    while True:
-        time.sleep(10)
-    thread1.stop()
-    thread2.stop()   
